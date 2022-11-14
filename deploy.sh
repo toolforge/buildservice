edd09907b2d173b57efb0823a3dfb5ef26e6af9e
komla <ssapaty@wikimedia.org>
2022-11-14 03:30:45 +0000

    default to prod, when no parameters are passed

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+
+
 help() {
     cat <<EOH
     Usage: $0 [OPTIONS] <ENVIRONMENT>
@@ -10,10 +12,24 @@ help() {
       -v        Show verbose output.
 EOH
 }
+
+
+check_environment() {
+    # verify that the proper environment is passed
+    local environment="${1?No environment passed}"
+
+    if [[ ! -d "deploy/$environment" || "$environment" =~ ^(base|README)$ ]]; then
+        echo "Unknown environment $environment, use one of:"
+        ls deploy/ | egrep -v '^(base|README)'
+        exit 1
+    fi
+}
+
+
 deploy_generic() {
     local environment="${1?No environment passed}"
 
-    if [[ "$environment" = "devel" ]]; then
+    if [[ "$environment" == "devel" ]]; then
         sed "s/{{HARBOR_IP}}/${HARBOR_IP}/"\
           deploy/devel/auth-patch.yaml.template > deploy/devel/auth-patch.yaml
     fi
@@ -23,10 +39,12 @@ deploy_generic() {
     else
         kubectl="kubectl"
     fi
-
+    
     $kubectl apply -k "deploy/base-tekton"
     $kubectl apply -k "deploy/$environment"
 }
+
+
 main () {
     while getopts "hv" option; do
         case "${option}" in
@@ -43,12 +61,20 @@ main () {
         esac
     done
     shift $((OPTIND-1))
-    local environment="${1?No environment passed}"
-    if [[ ! -d "deploy/$environment" || "$environment" =~ ^(base|README)$ ]]; then
-        echo "Unknown environment $environment, use one of:"
-        ls deploy/ | egrep -v '^(base|README)'
-        exit 1
+    
+    # default to prod, avoid deploying dev in prod if there's any issue
+    local environment="tools"
+    if [[ "${1:-}" == "" ]]; then
+        if [[ -f /etc/wmcs-project ]]; then
+            environment="$(cat /etc/wmcs-project)"
+        fi
+    else
+        environment="${1:-}"
     fi
+
+    check_environment "$environment"
     deploy_generic "$environment"
 }
+
+
 main "$@"
